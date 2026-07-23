4a5661e9381756ef02a340692de8d6c670a23f58
http-Ayush <mrayush8903@gmail.com>
2026-07-23 17:22:34 +0530

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json");
-echo json_encode([
-    "message" => "Welcome to Muskan Chatbot PHP API!",
-    "endpoints" => [
-        "POST /chat" => "Interact with Muskan by sending JSON body: {'message': 'Hello'}"
-    ]
-]);
